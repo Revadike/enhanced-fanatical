b4bfc73d4e539a426ecc783672e911c0872045c6
Raymond Law <raymondlaw@users.noreply.github.com>
2018-06-30 20:52:42 -0400

    Rename Packages to Applications
    applications = apps
    packages = subs

--- enhanced-fanatical.user.js
+++ enhanced-fanatical.user.js
@@ -2,13 +2,13 @@
 // @name         EnhancedFanatical
 // @namespace    EnhancedFanatical
 // @version      1.0.0
-// @description  Userscript that highlights owned packages on Fanatical.com bundles
+// @description  Userscript that highlights owned applications on Fanatical.com bundles
 // @author       Raymond Law - https://github.com/raymondlaw
 // @match        https://www.fanatical.com/*
 // @grant        GM.xmlHttpRequest
 // @run-at       document.idle
 // ==/UserScript==
-let user_packages = [];
+let user_apps = [];
 (function() {
     'use strict';
     (function(history){
@@ -18,7 +18,7 @@ let user_packages = [];
                 history.onpushstate({state: state});
             }
             // Perform on each AJAX page change
-            setTimeout(function(){filter_games(user_packages);},1500);
+            setTimeout(function(){filter_games(user_apps);},1500);
             return pushState.apply(history, arguments);
         };
     })(window.history);
@@ -26,24 +26,24 @@ let user_packages = [];
 })();
 
 async function main(){
-    user_packages = await get_steam_packages();
-    filter_games(user_packages);
+    user_apps = await get_steam_apps();
+    filter_games(user_apps);
 }
 
-function filter_games(user_packages){
+function filter_games(user_apps){
     let bundles = document.querySelectorAll('.bundle-accordion');
     let owned_games = [].filter.call(bundles , bundle => {
         let shop_link = bundle.querySelector('a[href*="store.steampowered.com/app/"]');
         if(shop_link == null) return false;
         let app_id = +(shop_link.href.substring(shop_link.href.indexOf('/app/')+5));
-        return user_packages.indexOf(app_id) > -1;
+        return user_apps.indexOf(app_id) > -1;
     });
     owned_games.map(owned_game => {
         owned_game.getElementsByClassName('bundle-row')[0].style.setProperty('background', 'linear-gradient(to right,  rgba(163,207,6,0.6) 0%,rgba(75,106,33,0.6) 100%)', 'important');
     });
 }
 
-function get_steam_packages(){
+function get_steam_apps(){
     return new Promise(resolve => {
         GM.xmlHttpRequest({
             method: "GET",
